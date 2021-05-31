fa8c43f9c01c10f7775b083890bcfd33a1f39fdc
Nathan Opresnik <nopresnik@me.com>
2021-05-31 21:28:18 +1000

    Create a user model

---
+++ src/models/user.model.ts
@@ -0,0 +1,17 @@
+import { Schema, model } from 'mongoose';
+
+export interface User {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const schema = new Schema<User>({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+const UserModel = model<User>('User', schema);
+
+export default UserModel;
