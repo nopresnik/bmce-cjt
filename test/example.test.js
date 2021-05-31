d996739cddddd10fa5648b45e12c5ec672d9445e
Nathan Opresnik <nopresnik@me.com>
2021-05-31 20:56:55 +1000

    Remove test.js file

--- test/example.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var chai_1 = require("chai");
-describe("Example Test", function () {
-    it("should add 1 + 1", function () {
-        chai_1.expect(1 + 1).to.equal(2);
-    });
-});
