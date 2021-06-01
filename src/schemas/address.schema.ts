65050e93413b5a569acfb1d86a07eb7cb4907af6
Nathan Opresnik <nopresnik@me.com>
2021-06-01 14:16:10 +1000

    Refactor address schema

--- src/schemas/address.schema.ts
+++ src/schemas/address.schema.ts
@@ -1,12 +1,10 @@
 import { Schema } from 'mongoose';
 import IAddress from '../types/IAddress';
 
-const schema = new Schema<IAddress>({
+export default new Schema<IAddress>({
   line1: { type: String },
   line2: { type: String },
   city: { type: String },
   state: { type: String },
   postcode: { type: String },
 });
-
-export default schema;
