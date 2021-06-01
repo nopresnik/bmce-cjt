5783c7d4188fbfa34a1ede13e3763f5e58911fc3
Nathan Opresnik <nopresnik@me.com>
2021-06-01 13:41:31 +1000

    Hash passwords

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 export interface User {
   name: string;
@@ -8,8 +9,19 @@ export interface User {
 
 const schema = new Schema<User>({
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
   password: { type: String, required: true },
+});
+
+schema.pre('save', function (next) {
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+      this.password = hash;
+      next();
+    });
+  });
 });
 
 const UserModel = model<User>('User', schema);
