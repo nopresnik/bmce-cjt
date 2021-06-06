dff738d874eac9bade70b6cd9715248651fb1a98
Nathan Opresnik <nopresnik@me.com>
2021-06-06 21:47:00 +1000

    Revert "Build new database connector"
    This reverts commit 1f24fa9fefe15070433a090e70c5798aea236278.

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
+import makeConnection from './models/connection';
 import createServer from './server';
-import * as db from './database';
 
 function startServer() {
   const port: number = parseInt(<string>process.env.PORT, 10) || 3001;
   const app = createServer();
 
-  db.connect()
-    .then(() => app.listen(port, () => console.log(`Server running on port ${port}`)))
+  makeConnection()
+    .then(() => {
+      app.listen(port, () => console.log(`Server running on port ${port}`));
+    })
     .catch(console.log);
 }
 
--- test/setup.spec.ts
+++ test/setup.spec.ts
@@ -1,11 +1,22 @@
-import * as db from 'database';
+import makeConnection from 'models/connection';
 
 const uri = 'mongodb://localhost/bmcjt-test';
 
-before(async () => {
-  await db.connect(uri);
+before((done) => {
+  makeConnection(uri).then(() => done());
 });
 
 after(async () => {
+  const db = await makeConnection(uri);
+  await db.connection.db.dropDatabase();
+  await db.connection.close();
   await db.disconnect();
+
+  // makeConnection(uri).then((db) => {
+  //   db.connection.db.dropDatabase(() => {
+  //     db.connection.close().then(() => {
+  //       db.disconnect().then(() => done());
+  //     });
+  //   });
+  // });
 });
