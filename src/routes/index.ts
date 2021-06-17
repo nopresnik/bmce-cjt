30d40b13e677dbab9f41aef423387cd469416c05
Nathan Opresnik <nopresnik@me.com>
2021-06-17 20:47:00 +1000

    Break the app to test Jenkins

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,11 +4,10 @@ import clientRoutes from './client.route';
 import jobRoutes from './job.route';
 import priceCategoriesRoutes from './pricecategory.route';
 import statsRoutes from './stats.route';
-import userRoutes from './user.route';
 
 const router = Router();
 
-router.use('/api/users', userRoutes);
+router.use('/api/users', userRoutess);
 
 router.use('/api/clients', clientRoutes);
 
