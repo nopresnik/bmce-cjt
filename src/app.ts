1c5e561948d62933215f2452034e47f67e2d302f
Nathan Opresnik <nopresnik@me.com>
2021-06-21 20:43:31 +1000

    Install socket.io, use http server

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,15 @@
+import http from 'http';
 import makeConnection from './models/connection';
 import createServer from './server';
 
 function startServer() {
   const port: number = parseInt(<string>process.env.PORT, 10) || 3001;
   const app = createServer();
+  const server = http.createServer(app);
 
   makeConnection()
     .then(() => {
-      app.listen(port, () => console.log(`Server running on port ${port}`));
+      server.listen(port, () => console.log(`Server running on port ${port}`));
     })
     .catch(console.log);
 }
