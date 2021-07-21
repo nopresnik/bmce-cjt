171c87cc0702e810b6443a8b5db14b9a48c9ff60
Nathan Opresnik <nopresnik@me.com>
2021-07-21 16:25:02 +1000

    Create job folder using path.join for better cross platform support.  Generate folder name more effectively.

--- src/helpers/foldergen.ts
+++ src/helpers/foldergen.ts
@@ -1,35 +1,36 @@
 import fs from 'fs';
-import Job from 'types/IJob';
+import path from 'path';
 import db from '../models';
+import Job from '../types/IJob';
 
 const NETWORK_LOCATION = process.env.BMCJT_NETWORK_LOCATION || '/Users/nopresnik/JOBS/';
 
 const FOLDERS = [
-  '01. Current Files/Received',
+  path.join('01. Current Files', 'Received'),
 
-  '02. Emails and Incoming Correspondence/DBYD',
-  '02. Emails and Incoming Correspondence/Quote',
+  path.join('02. Emails and Incoming Correspondence', 'DBYD'),
+  path.join('02. Emails and Incoming Correspondence', 'Quote'),
 
-  '03. Soil & Survey',
+  path.join('03. Soil & Survey'),
 
-  '04. Photos',
+  path.join('04. Photos'),
 
-  '05. Laboratory',
+  path.join('05. Laboratory'),
 
-  '06. Footing Design/01. Issued/A (XX-XX-XX)',
-  '06. Footing Design/01. Issued/B (XX-XX-XX)',
-  '06. Footing Design/02. Drawings',
+  path.join('06. Footing Design', '01. Issued', 'A (XX-XX-XX)'),
+  path.join('06. Footing Design', '01. Issued', 'B (XX-XX-XX)'),
+  path.join('06. Footing Design', '02. Drawings'),
 
-  '07. Structural/01. Issued/A (XX-XX-XX)',
-  '07. Structural/01. Issued/B (XX-XX-XX)',
-  '07. Structural/02. Drawings',
-  '07. Structural/03. Computations/00. Output',
-  '07. Structural/03. Computations/CADE',
-  '07. Structural/03. Computations/HYNE',
-  '07. Structural/03. Computations/SmartFrame',
-  '07. Structural/03. Computations/SpaceGass',
+  path.join('07. Structural', '01. Issued', 'A (XX-XX-XX)'),
+  path.join('07. Structural', '01. Issued', 'B (XX-XX-XX)'),
+  path.join('07. Structural', '02. Drawings'),
+  path.join('07. Structural', '03. Computations', '00. Output'),
+  path.join('07. Structural', '03. Computations', 'CADE'),
+  path.join('07. Structural', '03. Computations', 'HYNE'),
+  path.join('07. Structural', '03. Computations', 'SmartFrame'),
+  path.join('07. Structural', '03. Computations', 'SpaceGass'),
 
-  '08. Inspection',
+  path.join('08. Inspection'),
 ];
 
 const folderRangeString = (jobID: number, rounding: number) => {
@@ -49,15 +50,21 @@ const folderRangeString = (jobID: number, rounding: number) => {
 const makeJobFolder = async (job: Job): Promise<void> => {
   const client = await db.Client.findOne({ _id: job.client });
 
-  const buildName = `${job.jobID} ${client?.name} ${job.location.line1 || ''} ${job.location.line2 || ''} ${
-    job.location.city || ''
-  } ${job.location.state || ''} ${job.location.postcode || ''}`.trim();
+  const buildName: (string | number)[] = [];
 
-  const jobFolder = NETWORK_LOCATION + `${folderRangeString(job.jobID, 1000)}/${buildName}/`.trim();
+  buildName.push(job.jobID);
+  if (client?.name) buildName.push(client?.name.trim());
+  if (job.location.line1) buildName.push(job.location.line1.trim());
+  if (job.location.line2) buildName.push(job.location.line2.trim());
+  if (job.location.city) buildName.push(job.location.city.trim());
+  if (job.location.state) buildName.push(job.location.state.trim());
+  if (job.location.postcode) buildName.push(job.location.postcode.trim());
+
+  const jobFolder = path.join(NETWORK_LOCATION + folderRangeString(job.jobID, 1000), buildName.join(' '));
 
   if (!fs.existsSync(jobFolder)) {
     FOLDERS.forEach((folder) => {
-      fs.mkdirSync(jobFolder.trim() + folder, { recursive: true });
+      fs.mkdirSync(path.join(jobFolder, folder), { recursive: true });
     });
   }
 };
